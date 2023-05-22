8b4639760da2254f791875c715291601da74874c
Victor Caio <kaioaraujo426@gmail.com>
2023-05-22 01:35:58 -0300

    FEAT: created routes to CRUD

--- server/src/routes/memories.routes.ts
+++ server/src/routes/memories.routes.ts
@@ -3,7 +3,26 @@ import { z } from 'zod';
 import { prismaClient } from '../lib/prisma';
 
 export async function memoriesRoutes(app: FastifyInstance) {
-  app.post('/memories', async () => {});
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      is_public: z.coerce.boolean().default(false),
+    });
+
+    const { content, coverUrl, is_public } = bodySchema.parse(request.body);
+
+    const memory = await prismaClient.memory.create({
+      data: {
+        content,
+        cover_url: coverUrl,
+        is_public,
+        user_id: 'd90910fe-e6ff-4f98-a6c5-31bac397f851',
+      },
+    });
+
+    return memory;
+  });
 
   app.get('/memories', async () => {
     const memories = await prismaClient.memory.findMany({
@@ -35,7 +54,76 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory;
   });
 
-  app.put('/memories/:id', async () => {});
+  app.put('/memories/:id', async (request, response) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
 
-  app.delete('/memories/:id', async () => {});
+    const paramsVerifyZod = paramsSchema.safeParse(request.params);
+
+    if (!paramsVerifyZod.success) {
+      return response.status(400).send({ ERROR: 'Invalid ID!' });
+    }
+
+    let memoryExists;
+
+    try {
+      memoryExists = await prismaClient.memory.findUniqueOrThrow({
+        where: { id: paramsVerifyZod.data.id },
+      });
+    } catch (error) {
+      return response.status(404).send({
+        ERROR: 'Memory does not exists!',
+      });
+    }
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      is_public: z.coerce.boolean().default(false),
+    });
+
+    const bodyVerifyZod = bodySchema.safeParse(request.body);
+
+    if (!bodyVerifyZod.success) {
+      return response.status(400).send({ ERROR: bodyVerifyZod.error.errors });
+    }
+
+    const memoryUpdated = await prismaClient.memory.update({
+      where: { id: paramsVerifyZod.data.id },
+      data: {
+        content: bodyVerifyZod.data.content,
+        cover_url: bodyVerifyZod.data.coverUrl,
+        is_public: bodyVerifyZod.data.is_public,
+      },
+    });
+
+    return memoryUpdated;
+  });
+
+  app.delete('/memories/:id', async (request, response) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const zodVerification = paramsSchema.safeParse(request.params);
+
+    if (!zodVerification.success) {
+      return response.status(400).send({ ERROR: 'Invalid ID!' });
+    }
+
+    try {
+      await prismaClient.memory.findUniqueOrThrow({
+        where: { id: zodVerification.data.id },
+      });
+
+      await prismaClient.memory.delete({
+        where: { id: zodVerification.data.id },
+      });
+    } catch (error: any) {
+      return response.status(404).send({
+        ERROR: 'Memory does not exists!',
+      });
+    }
+  });
 }
